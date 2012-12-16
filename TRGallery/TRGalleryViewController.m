7529dfd68a4057548eebdcb67b11882aa4fc86f0
Garrett Richards <garrettrichards@gmail.com>
2012-12-15 16:00:44 -0800

    initial commit

---
+++ TRGallery/TRGalleryViewController.m
@@ -0,0 +1,38 @@
+//
+//  TRGalleryViewController.m
+//  TRGallery
+//
+//  Created by Garrett Richards on 12/15/12.
+//  Copyright (c) 2012 Trulia. All rights reserved.
+//
+
+#import "TRGalleryViewController.h"
+
+@interface TRGalleryViewController ()
+
+@end
+
+@implementation TRGalleryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
